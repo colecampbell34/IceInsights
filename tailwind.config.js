4dd859130f8fe295326da53481aa1514e1ff43d9
Cole Campbell <ccampbell211@my.bcit.ca>
2025-01-28 10:04:16 -0800

    continued to troubleshoot with tailwindcss, still not working

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
     content: [
       "./src/**/*.{js,jsx,ts,tsx}", // Adjust for your file types and locations
+      "./public/index.html"
     ],
     theme: {
       extend: {},
